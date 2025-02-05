5661ab692b6c1cdb41d779f16c0216ce78e7913a
Vinícius Landim <lviniciusaugusto@hotmail.com>
2025-02-05 13:56:35 -0300

    SpreadGun Looking Up Partial Fix
    - Falta rotacionar o sprite da bala

--- Assets/Script/Guns Scripts/Spread Gun/SpreadGunScript.cs
+++ Assets/Script/Guns Scripts/Spread Gun/SpreadGunScript.cs
@@ -15,6 +15,7 @@ public class SpreadGun : Gun
 
         // angle for the extreme bullet below the origin bullet
         float startAngle = -spreadAngle / 2f;
+        Vector2 directionVector;
 
         for (int i = 0; i < bulletCount; i++)
         {
@@ -31,10 +32,17 @@ public class SpreadGun : Gun
             // calculating bullet's direction
             //(cos(x), sin(x)) describes the position of a point on a unit circle; direction is always 1 or -1 (player.transform.localScale.x)
             // Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * direction gets the directional vector that the bullet will follow
-            Vector2 directionVector = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * direction;
-            bullet.transform.localScale = new Vector3(direction * Mathf.Abs(bullet.transform.localScale.x),
-                                                      bullet.transform.localScale.y,
-                                                      bullet.transform.localScale.z);
+            if (playerController != null && playerController.IsLookingUp)
+            {
+                directionVector = new Vector2(Mathf.Sin(radians), Mathf.Cos(radians));
+            }
+            else
+            {
+                directionVector = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * direction;
+                bullet.transform.localScale = new Vector3(direction * Mathf.Abs(bullet.transform.localScale.x),
+                                                          bullet.transform.localScale.y,
+                                                          bullet.transform.localScale.z);
+            }
 
             Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
             bulletRB.linearVelocity = directionVector * shotSpeed;
